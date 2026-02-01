4ccff648b30ca9557b70a5574d1c345780cf8b26
Ram Bapu <rambapu270197@gmail.com>
2026-02-01 21:56:56 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json for project dependencies and scripts
    - Included Vite logo SVG in public directory
    - Created App.css for styling the application
    - Developed App.jsx as the main application component
    - Added React logo SVG in assets directory
    - Established global styles in index.css
    - Set up main entry point in main.jsx
    - Configured Vite with vite.config.js for React support

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+import { ENV } from "./config/env";
+
+const app = express();
+
+const PORT = ENV.PORT;
+
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true })); // allows request from frontend with cookies
+app.use(clerkMiddleware()); // auth obj attached to req
+app.use(express.json({ limit: "5mb" })); // Parses json in request body
+app.use(express.urlencoded({ extended: true })); // parses form data (like HTML forms)
+
+app.get("/", (req, res) => {
+  res.json({
+    message:
+      "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Health check passed" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on port:", PORT);
+});
